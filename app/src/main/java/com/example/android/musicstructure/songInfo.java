e51e20bfba6d270feeb652dcb23ce05d395177a9
stupidhero <33969192+stupidhero@users.noreply.github.com>
2018-04-08 13:56:14 +0200

    Implemented the required changes as stated in the Review

--- app/src/main/java/com/example/android/musicstructure/songInfo.java
+++ app/src/main/java/com/example/android/musicstructure/songInfo.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.text.format.DateUtils;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class songInfo extends AppCompatActivity {
@@ -30,5 +32,19 @@ public class songInfo extends AppCompatActivity {
 
         TextView funFactView = findViewById(R.id.funFactView);
         funFactView.setText(song.getFact());
+
+        Button returnButton = findViewById(R.id.backToList);
+
+        returnButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View View) {
+
+                Intent returnToList = new Intent(songInfo.this, MainActivity.class);
+
+                //return to Main Activity
+                startActivity(returnToList);
+            }
+        });
     }
 }
